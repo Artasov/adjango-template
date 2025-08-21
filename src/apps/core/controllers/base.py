e6e300d79127c39b34255da90e1b8f65dac76ca6
Artasov <ivanhvalevskey@gmail.com>
2025-08-21 08:31:45 +0300

    process

--- src/apps/core/controllers/base.py
+++ src/apps/core/controllers/base.py
@@ -4,13 +4,14 @@ from rest_framework.response import Response
 
 # Sync example controller
 @api_view(('GET',))
-def example(request):
+def example(_request):
     return Response({'message': 'example success'})
 
 
+# ASync example controller
 from adrf.decorators import api_view
 
 
 @api_view(('GET',))
-async def async_example(request):
+async def async_example(_request):
     return Response({'message': 'async example success'})
